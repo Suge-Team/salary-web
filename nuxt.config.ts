f5bb8aaf23e59333c3a664db76bfa83d795c62ae
Nguyễn Anh Tuấn <tuanna.hsp@gmail.com>
2023-01-03 16:17:36 +0900

    chore: set up some dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  typescript: {
+    shim: false,
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+    useStylesheet: false,
+  },
+});
