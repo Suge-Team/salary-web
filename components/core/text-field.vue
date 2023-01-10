77d2678f86e38e76edd647047e3524aced24629a
Nguyễn Anh Tuấn <tuanna.hsp@gmail.com>
2023-01-11 00:47:25 +0900

    feat: add salary page basic layout

---
+++ components/core/text-field.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label v-if="!!label" :for="name" class="block text-sm font-medium text-gray-700">{{ label }}</label>
+    <div class="mt-1">
+      <input
+        :id="name"
+        :type="type"
+        :name="name"
+        class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: String,
+  type: { type: String, default: "text" },
+  name: String,
+  placeholder: { type: String, default: "" },
+  modelValue: [String, Number],
+});
+
+const emits = defineEmits(["update:modelValue"]);
+</script>
