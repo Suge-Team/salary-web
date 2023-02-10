b1671dc64c120bcad6206553f1b3ee98df9e6216
Anh Tuan <tuanna.hsp@gmail.com>
2023-02-10 09:03:37 +0900

    feat: api integration for the companies page

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,8 @@ module.exports = {
     "vue/require-default-prop": "off",
     "vue/multi-word-component-names": "off",
   },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
 };
--- utils/sort.js
+++ utils/sort.js
@@ -2,3 +2,15 @@ export const SortOrder = {
   DESC: "desc",
   ASC: "asc",
 };
+
+export function sortCollection(collection, sortBy, sortOrder) {
+  return collection.sort((a, b) => {
+    if (typeof a[sortBy] === "number") {
+      return sortOrder === SortOrder.ASC ? a[sortBy] - b[sortBy] : b[sortBy] - a[sortBy];
+    }
+
+    if (typeof a[sortBy] === "string") {
+      return sortOrder === SortOrder.ASC ? a[sortBy].localeCompare(b[sortBy]) : b[sortBy].localeCompare(a[sortBy]);
+    }
+  });
+}
