c7a33a6bca186aea28b64c161aa1c5ac840018d8
tuna-date <anhtu.hspk61@gmail.com>
2023-02-12 00:32:38 +0900

    feat: setup google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,7 @@ export default defineNuxtConfig({
     },
   },
   modules: ["@nuxtjs/google-fonts"],
+  plugins: ['~/plugins/vue-gtag.client.ts'],
   googleFonts: {
     families: {
       Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
