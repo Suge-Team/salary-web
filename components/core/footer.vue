7e474c8bd69b1ab87c669f0b6e81412994b9ee35
Anh Tuan <tuanna.hsp@gmail.com>
2023-03-09 20:49:56 +0900

    fix: feedback form location

--- components/core/footer.vue
+++ components/core/footer.vue
@@ -3,11 +3,9 @@
     <div class="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
       <div class="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
         <span class="block sm:inline">&copy; 2023 Suge Team.</span>
-        <span class="block sm:inline"> All rights reserved.</span>
+        <span class="block sm:inline"> All rights reserved. </span>
         <span class="block sm:inline">
-          <a class="decoration-solid underline" href="https://forms.gle/GNgG2XH5NefCJEDR6" target="_blank">
-            Contact us
-          </a>
+          <nuxt-link class="decoration-solid text-primary underline" to="/about">Contact us</nuxt-link>
         </span>
       </div>
     </div>
